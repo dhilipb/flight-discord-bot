f3ed55261f7a24cb52d1f726fb7758c001af80e6
Dhilip Buddha <dhilipb@gmail.com>
2022-09-12 22:52:51 +0100

    Update check

--- index.js
+++ index.js
@@ -7,37 +7,37 @@ const MessageGenerator = require('./src/message-generator');
 
 if (!process.env.BOT_TOKEN || !process.env.BOT_CLIENT) {
     console.error('BOT_TOKEN and BOT_CLIENT are required');
-    return;
+} else {
+    DiscordClient.on('ready', async () => {
+        initBot();
+        console.log(`Logged in as ${DiscordClient.user.tag}!`);
+
+        await TrackManager.trackToday();
+        setInterval(async () => {
+            await TrackManager.trackToday();
+        }, (process.env.CHECK_FREQUENCY_MINS || 3) * 60 * 1000);
+    });
+
+    DiscordClient.on('interactionCreate', async (interaction) => {
+        if (!interaction.isChatInputCommand()) return;
+
+        const { commandName } = interaction;
+
+        if (commandName === 'ping') {
+            await interaction.reply('Pong!');
+        } else if (commandName === 'track') {
+            await TrackManager.addToTrack(interaction);
+            await TrackManager.trackToday();
+        } else if (commandName === 'check') {
+            const replyText = await MessageGenerator.get(flight);
+            interaction.reply({ embeds: [replyText] });
+        }
+    });
+
+    DiscordClient.login(process.env.BOT_TOKEN);
 }
 
-DiscordClient.on('ready', async () => {
-    initBot();
-    console.log(`Logged in as ${DiscordClient.user.tag}!`);
-
-    await TrackManager.trackToday();
-    setInterval(async () => {
-        await TrackManager.trackToday();
-    }, (process.env.CHECK_FREQUENCY_MINS || 3) * 60 * 1000);
-});
-
-DiscordClient.on('interactionCreate', async (interaction) => {
-    if (!interaction.isChatInputCommand()) return;
-
-    const { commandName } = interaction;
-
-    if (commandName === 'ping') {
-        await interaction.reply('Pong!');
-    } else if (commandName === 'track') {
-        await TrackManager.addToTrack(interaction);
-        await TrackManager.trackToday();
-    } else if (commandName === 'check') {
-        const replyText = await MessageGenerator.get(flight);
-        interaction.reply({ embeds: [replyText] });
-    }
-});
-
-DiscordClient.login(process.env.BOT_TOKEN);
-
+// Clear console
 // process.stdout.write("\u001b[3J\u001b[2J\u001b[1J");
 // console.clear();
 
